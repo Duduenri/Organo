a0381c4d32fc1dfcbe5f8ff0bf1daa0e706643ed
Duduenri <eduess290799@gmail.com>
2022-10-18 17:16:51 -0300

    Fixed form errors

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -6,7 +6,7 @@ const Form = () => {
         <section className='form'>
             <form>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <Textfieldeld label="Nome" placeholder="Digite seu nome" />
+                <Textfield label="Nome" placeholder="Digite seu nome" />
                 <Textfield label="Cargo" placeholder="Digite seu cargo" />
                 <Textfield label="Imagem" placeholder="Digite o endereço da imagem" />
             </form>
