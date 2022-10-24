0a51673b0453a8dff587d7fa27b1661625a7439d
Duduenri <eduess290799@gmail.com>
2022-10-24 20:08:04 -0300

    ciclo de vida testes

--- src/components/Textfield/index.js
+++ src/components/Textfield/index.js
@@ -1,12 +1,19 @@
 import './Textfield.css'
 
 const Textfield = (props) => {
+    let valor = ''
+
+    const aoDigitado = (evento) => {
+        valor = evento.targent.value
+        console.log(valor) 
+    }
+
     return(
         <div className="text-field">
             <label>
                 {props.label}
             </label>
-            <input required={props.orbigatorio} placeholder={props.placeholder}/>
+            <input value={valor} onChange={aoDigitado} required={props.orbigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
